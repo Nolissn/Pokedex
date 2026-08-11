b8ea282a3c8b8050763fd89cbf3f293f6acbf3ea
Luis Simon <nolissn22222@gmail.com>
2026-08-11 23:25:32 +0200

    feat: added a loop function in order to render all 20 pokemon cards for a section and replaced the dummy picture with the correct image for every pokemon. Also added a grid via CSS for the section layout

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
-function init(){
-    loadTwentyPokemons(0, 20);
+function init() {
+  loadTwentyPokemons(0, 20);
 }
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+let sectioncounter = 0;
 
-async function loadTwentyPokemons(start, end){
-   let response = await fetch(`${BASE_URL}?limit=${end}&offset=${start}`);
-   let information = await response.json();
-   loadOnePokemonSection();
+async function loadTwentyPokemons(start, end) {
+  let response = await fetch(`${BASE_URL}?limit=${end}&offset=${start}`);
+  let information = await response.json();
+  console.log(information);
+  loadOnePokemonSection(information);
 }
 
-function loadOnePokemonSection(){
-    main = document.getElementById('mainId');
-    main.innerHTML = getOnePokemonSectionTemplate();
+function loadOnePokemonSection(information) {
+  sectioncounter++;
+  let main = document.getElementById("mainId");
+  main.innerHTML = getOnePokemonSectionTemplate(sectioncounter);
+  let currentsection = document.getElementById(
+    "twenty_pokemons_section_" + sectioncounter,
+  );
+
+  for (let index = 1; index < information.results.length + 1; index++) {
+    const pokemon = information.results[index - 1];
+    currentsection.innerHTML += getPokemonCardInSectionTemplate(index, pokemon);
+  }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,18 +1,19 @@
-function getOnePokemonSectionTemplate() {
-  return /* HTML */ ` <section class="twenty_pokemons_section">
-    <button class="small_pokemon_card">
-      <header>
-        <img
-          class="small_card_image"
-          src="./assets/SVG's/pokemon_card_image_template.svg"
-          alt="Pokémon Image"
-        />
-      </header>
-      <span>NAME</span>
-      <section>
-        <div></div>
-      </section>
-      <span>ID</span>
-    </button>
-  </section>`;
+function getOnePokemonSectionTemplate(sectioncounter) {
+  return /* HTML */ ` <section id="twenty_pokemons_section_${sectioncounter}" class="twenty_pokemons_section"></section> `;
+}
+
+function getPokemonCardInSectionTemplate(index, pokemon) {
+  return /* HTML */ `<button class="small_pokemon_card">
+    <img
+      class="small_card_image"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index}.png"
+      alt="Pokémon Image"
+    />
+    <span class="pokemon_card_name">${pokemon.name.toUpperCase()}</span>
+    <section class="pokemon_type_section">
+      <div class="pokemon_card_type_general">Type</div>
+      <div class="pokemon_card_type_general">Type</div>
+    </section>
+    <span class="pokemon_card_id">#0000</span>
+  </button>`;
 }
