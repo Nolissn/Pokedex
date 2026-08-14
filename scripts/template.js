90ad26d8fac684de4d5f3fd63dc0e12a029bde89
Luis Simon <nolissn22222@gmail.com>
2026-08-14 14:05:55 +0200

    fix: found a way to calculate the font size of th names depending on the lenght of it using clamp and calc. Also made a line break impossible to ensure the Layout doesnt break.
    Additionally I removed the repeating background image because it would be a ripoff of pokemon.com

--- scripts/template.js
+++ scripts/template.js
@@ -7,7 +7,7 @@ function getPokemonCardInSectionTemplate(pokemonDetails) {
       src="${pokemonDetails.sprites.other["official-artwork"].front_default}"
       alt="Pokémon Image"
     />
-    <span class="pokemon_card_name">${makeFirstCharUpperCase(pokemonDetails.name)}</span>
+    <span class="pokemon_card_name" style="--name_length: ${pokemonDetails.name.length}">${makeFirstCharUpperCase(pokemonDetails.name)}</span>
     <section class="pokemon_type_section">
       <div class="pokemon_card_type_general ${pokemonDetails.types[0].type.name}">${makeFirstCharUpperCase(pokemonDetails.types[0].type.name)}</div>
       ${pokemonDetails.types.length === 2 ? `<div class="pokemon_card_type_general ${pokemonDetails.types[1].type.name}">${makeFirstCharUpperCase(pokemonDetails.types[1].type.name)}</div>` : ""}
