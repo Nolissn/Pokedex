5182b322a42df87c56df7993b3e05afa1ed980a1
Luis Simon <nolissn22222@gmail.com>
2026-08-15 17:40:40 +0200

    feat: added spinning loading icon and refactored the code by getting rid of custom_helper_functions.js

--- scripts.js
+++ scripts.js
@@ -5,11 +5,16 @@ function init() {
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 
 async function loadPokemons(start, amound) {
-  let response = await fetch(`${BASE_URL}?limit=${amound}&offset=${start - 1}`);
-  let basicInformation = await response.json();
-  console.log(basicInformation);
-  await loadOnePokemonSection(basicInformation);
+  openLoadingSpinner();
+  try {
+    let response = await fetch(`${BASE_URL}?limit=${amound}&offset=${start - 1}`);
+    let basicInformation = await response.json();
+    await loadOnePokemonSection(basicInformation);
+  } finally {
+    closeLoadingSpinner();
+  }
 }
+
 
 async function loadOnePokemonSection(basicInformation) {
   let section = document.getElementById("pokemon_section");
@@ -17,7 +22,31 @@ async function loadOnePokemonSection(basicInformation) {
   for (let index = 0; index < basicInformation.results.length; index++) {
     let detailResponse = await fetch(basicInformation.results[index].url);
     let pokemonDetails = await detailResponse.json();
-    console.log(pokemonDetails);
     section.innerHTML += getPokemonCardInSectionTemplate(pokemonDetails);
   }
+}
+
+function openLoadingSpinner() {
+  const loadingSpinner = document.getElementById("loadingScreenDialogId");
+  loadingSpinner.showModal();
+}
+
+function closeLoadingSpinner() {
+  const loadingSpinner = document.getElementById("loadingScreenDialogId");
+  loadingSpinner.close();
+}
+
+function makeFirstCharUpperCase(rawstring) {
+  rawstring = rawstring.charAt(0).toUpperCase() + rawstring.slice(1);
+  return rawstring;
+}
+
+function findLastLoadedPokemonId() {
+  const section = document.getElementById("pokemon_section");
+  let numb = section.childNodes.length;
+  let specificButton = document.getElementById("pokemonCardId" + numb);
+  let specificID = specificButton.lastElementChild.innerHTML;
+  specificID = specificID.replaceAll("#", "");
+  specificID = parseInt(specificID);
+  return specificID;
 }
